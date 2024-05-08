06b8190ca404d34eb0a8f17630465aeeb89dd7c4
abraham <abesihomb@gmail.com>
2024-05-08 20:27:56 +0700

    Experiment 1.2: Understanding how it works

--- timer/src/main.rs
+++ timer/src/main.rs
@@ -101,6 +101,7 @@ fn main() {
         println!("Abrahams’s Komputer: done!");
     });
 
+    println!("Abrahams’s Komputer: hey hey!");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
